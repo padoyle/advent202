6603d1348c8eb949da297aa7205b40b770b24b0e
Paul Doyle <pauldoyle22@gmail.com>
2020-12-03 23:04:05 -0800

    Day 4 scaffolding

---
+++ src/day_04.rs
@@ -0,0 +1,40 @@
+static INPUT: &'static str = include_str!("assets/day_04_input.txt");
+
+pub fn p1() -> usize {
+    0
+}
+
+pub fn p2() -> usize {
+    0
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    static EXAMPLE: &'static str = r#"ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+byr:1937 iyr:2017 cid:147 hgt:183cm
+
+iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+hcl:#cfa07d byr:1929
+
+hcl:#ae17e1 iyr:2013
+eyr:2024
+ecl:brn pid:760753108 byr:1931
+hgt:179cm
+
+hcl:#cfa07d eyr:2025 pid:166559648
+iyr:2011 ecl:brn hgt:59in"#;
+
+    #[test]
+    fn p1_example() {}
+
+    #[test]
+    fn p1_correct_answer() {}
+
+    #[test]
+    fn p2_example() {}
+
+    #[test]
+    fn p2_correct_answer() {}
+}
--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,17 @@ use std::env::args;
 mod day_01;
 mod day_02;
 mod day_03;
+mod day_04;
 
 fn main() {
     let args: Vec<String> = args().collect();
     let day: u32 = args[1].parse().expect("Usage:\n\tadvent2020 <day>");
 
     let answer = match day {
-        1 => {
-            format!("P1:\n{}\n\nP2:\n{}", day_01::p1(), day_01::p2())
-        }
-        2 => {
-            format!("P1:\n{}\n\nP2:\n{}", day_02::p1(), day_02::p2())
-        }
-        3 => {
-            format!("P1:\n{}\n\nP2:\n{}", day_03::p1(), day_03::p2())
-        }
+        1 => format!("P1:\n{}\n\nP2:\n{}", day_01::p1(), day_01::p2()),
+        2 => format!("P1:\n{}\n\nP2:\n{}", day_02::p1(), day_02::p2()),
+        3 => format!("P1:\n{}\n\nP2:\n{}", day_03::p1(), day_03::p2()),
+        4 => format!("P1:\n{}\n\nP2:\n{}", day_04::p1(), day_03::p2()),
         _ => "".to_string(),
     };
 
