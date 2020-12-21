d5d0af253e10dc474809570fcb0f50c12e6ee104
Paul Doyle <pauldoyle22@gmail.com>
2020-12-20 21:47:37 -0800

    Almost got it working on the exmaple....

--- src/day_20.rs
+++ src/day_20.rs
@@ -5,15 +5,34 @@ use std::{
 
 static INPUT: &str = include_str!("assets/day_20_input.txt");
 
+// We parse the sides clockwise
 static TOP: usize = 0;
-static BOT: usize = 0;
-static LEFT: usize = 0;
-static RIGHT: usize = 0;
+static RIGHT: usize = 1;
+static BOT: usize = 2;
+static LEFT: usize = 3;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Tile {
     id: u16,
     sides: [u16; 4],
+}
+
+impl Tile {
+    fn top_reversed(&self) -> u16 {
+        reverse_10_bits(self.sides[TOP])
+    }
+
+    fn right_reversed(&self) -> u16 {
+        reverse_10_bits(self.sides[RIGHT])
+    }
+
+    fn bot_reversed(&self) -> u16 {
+        reverse_10_bits(self.sides[BOT])
+    }
+
+    fn left_reversed(&self) -> u16 {
+        reverse_10_bits(self.sides[LEFT])
+    }
 }
 
 fn reverse_10_bits(value: u16) -> u16 {
@@ -26,13 +45,13 @@ fn reverse_10_bits(value: u16) -> u16 {
     new_value
 }
 
-fn precalculate_reverse_lookup() -> HashMap<u16, u16> {
-    let mut map = HashMap::new();
-    for i in 0..0b1111111111 {
-        map.insert(i, reverse_10_bits(i));
-    }
-    map
-}
+// fn precalculate_reverse_lookup() -> HashMap<u16, u16> {
+//     let mut map = HashMap::new();
+//     for i in 0..0b1111111111 {
+//         map.insert(i, reverse_10_bits(i));
+//     }
+//     map
+// }
 
 impl Tile {
     fn parse(input: &str) -> Self {
@@ -60,14 +79,14 @@ impl Tile {
         sides[0] = tile_lines.first().unwrap().chars().fold(0, fold_bits) >> 1;
         sides[1] = tile_lines
             .iter()
-            .map(|line| line.chars().next().unwrap())
+            .map(|line| line.chars().last().unwrap())
             .fold(0, fold_bits)
             >> 1;
         sides[2] = tile_lines.last().unwrap().chars().fold(0, fold_bits) >> 1;
         sides[3] = tile_lines
             .iter()
             .rev()
-            .map(|line| line.chars().last().unwrap())
+            .map(|line| line.chars().next().unwrap())
             .fold(0, fold_bits)
             >> 1;
 
@@ -88,45 +107,53 @@ impl Tile {
     fn flip(&mut self, vertical: bool, horizontal: bool) {
         let mut new_sides = self.sides.clone();
         if vertical {
+            // swap top & bottom sides
+            new_sides[TOP] = self.sides[BOT];
+            new_sides[BOT] = self.sides[TOP];
+            // reverse left & right sides
             new_sides[LEFT] = reverse_10_bits(self.sides[LEFT]);
             new_sides[RIGHT] = reverse_10_bits(self.sides[RIGHT]);
         }
         if horizontal {
+            // swap left & right sides
+            new_sides[LEFT] = self.sides[RIGHT];
+            new_sides[RIGHT] = self.sides[LEFT];
+            // reverse top & bottom sides
             new_sides[TOP] = reverse_10_bits(self.sides[TOP]);
             new_sides[BOT] = reverse_10_bits(self.sides[BOT]);
         }
     }
 
     fn check_fit(&self, neighbors: &[Option<u16>]) -> bool {
-        (0..3).all(|i| match neighbors[i] {
+        (0..4).all(|i| match neighbors[i] {
             Some(needed) => self.sides[i] == needed,
             None => true,
         })
     }
 
     fn find_fit(&mut self, neighbors: &[Option<u16>]) -> bool {
-        for _ in 0..3 {
+        for _ in 0..4 {
             if self.check_fit(neighbors) {
                 return true;
             }
             self.rotate(1);
         }
         self.flip(true, false);
-        for _ in 0..3 {
+        for _ in 0..4 {
             if self.check_fit(neighbors) {
                 return true;
             }
             self.rotate(1);
         }
         self.flip(false, true);
-        for _ in 0..3 {
+        for _ in 0..4 {
             if self.check_fit(neighbors) {
                 return true;
             }
             self.rotate(1);
         }
         self.flip(true, false);
-        for _ in 0..3 {
+        for _ in 0..4 {
             if self.check_fit(neighbors) {
                 return true;
             }
@@ -135,14 +162,6 @@ impl Tile {
 
         false
     }
-}
-
-fn fits_h(left: &Tile, right: &Tile) -> bool {
-    left.sides[RIGHT] == reverse_10_bits(right.sides[LEFT])
-}
-
-fn fits_v(top: &Tile, bot: &Tile) -> bool {
-    top.sides[BOT] == reverse_10_bits(bot.sides[TOP])
 }
 
 fn parse_tiles(input: &str) -> Vec<Tile> {
@@ -172,25 +191,108 @@ impl<'a> TileNode<'a> {
     }
 }
 
-// fn solve<'a>(tiles: &'a mut Vec<Tile>) -> TileNode<'a> {
-//     let mut remaining: Vec<&mut Tile> = tiles.iter_mut().collect();
-//     let dim = (tiles.len() as f64).sqrt() as usize;
-//     let mut min_x = 0;
-//     let mut min_y = 0;
-//     let mut max_x = 0;
-//     let mut max_y = 0;
-//     let mut root = TileNode::new(remaining.pop().unwrap());
+type TileMap = HashMap<(i32, i32), Tile>;
 
-//     let mut node: TileNode = root;
-//     while remaining.len() > 0 {
-//         // try assign left neighbor
-//         for tile in remaining {
+fn debug_print_map(map: &TileMap) {
+    let mut min_x = 0;
+    let mut max_x = 0;
+    let mut min_y = 0;
+    let mut max_y = 0;
+    map.keys().for_each(|&(x, y)| {
+        min_x = std::cmp::min(x, min_x);
+        max_x = std::cmp::max(x, max_x);
+        min_y = std::cmp::min(y, min_y);
+        max_y = std::cmp::max(y, max_y);
+    });
+    for x in min_x..max_x + 1 {
+        println!();
+        for y in min_y..max_y + 1 {
+            match map.get(&(x, y)) {
+                Some(tile) => print!("{} ", tile.id),
+                None => print!("____ "),
+            }
+        }
+    }
+    println!();
+}
 
-//         }
-//     }
+fn find_open_positions(image: &TileMap) -> HashSet<(i32, i32)> {
+    let mut results = HashSet::new();
+    for ((x, y), _) in image.iter() {
+        if image.get(&(x - 1, *y)).is_none() {
+            results.insert((x - 1, *y));
+        }
+        if image.get(&(x + 1, *y)).is_none() {
+            results.insert((x + 1, *y));
+        }
+        if image.get(&(*x, y - 1)).is_none() {
+            results.insert((*x, y - 1));
+        }
+        if image.get(&(*x, y + 1)).is_none() {
+            results.insert((*x, y + 1));
+        }
+    }
+    println!("{} possible positions to check", results.len());
+    results
+}
 
-//     root
-// }
+fn solve(tiles: Vec<Tile>) -> TileMap {
+    let mut remaining: HashMap<u16, Tile> =
+        tiles.iter().map(|tile| (tile.id, tile.clone())).collect();
+    // let dim = (tiles.len() as f64).sqrt() as usize;
+    // let mut min_x = 0;
+    // let mut min_y = 0;
+    // let mut max_x = 0;
+    // let mut max_y = 0;
+    let mut image: TileMap = HashMap::new();
+    let first_tile_key = remaining.keys().next().unwrap().clone();
+    let first_tile = remaining.remove(&first_tile_key).unwrap();
+    println!("Place {} at (0, 0)", first_tile_key);
+    image.insert((0, 0), first_tile);
+
+    let mut loop_count = 0;
+    loop {
+        println!("{} remaining", remaining.len());
+        for (next_x, next_y) in find_open_positions(&image) {
+            println!("Attempt to place tile at ({}, {})", next_x, next_y);
+            let neighbors: Vec<Option<u16>> = vec![
+                image.get(&(next_x, next_y - 1)).map(Tile::bot_reversed),
+                image.get(&(next_x + 1, next_y)).map(Tile::left_reversed),
+                image.get(&(next_x, next_y + 1)).map(Tile::top_reversed),
+                image.get(&(next_x - 1, next_y)).map(Tile::right_reversed),
+            ];
+            println!("Neighbors to match: {:?}", neighbors);
+
+            let mut found_id: Option<u16> = None;
+            for (id, tile) in remaining.iter_mut() {
+                println!("Check tile {}", tile.id);
+                if tile.find_fit(neighbors.as_slice()) {
+                    println!("\tTile {} fits!", id);
+                    found_id = Some(*id);
+                    break;
+                }
+            }
+
+            if let Some(id) = found_id {
+                let tile = remaining.remove(&id);
+                image.insert((next_x, next_y), tile.unwrap());
+                break;
+            }
+        }
+
+        loop_count += 1;
+        if loop_count >= 10 {
+            println!();
+            debug_print_map(&image);
+            panic!("NO MORE LOOOPING");
+        }
+        // if remaining.is_empty() {
+        //     break;
+        // }
+    }
+
+    image
+}
 
 pub fn p1() -> u64 {
     0
@@ -222,11 +324,8 @@ mod test {
         for tile in tiles.iter() {
             println!("{:?}", tile);
         }
-        let corners = find_unmatchable_corners(&tiles);
-        assert_eq!(
-            20899048083289u64,
-            corners.iter().map(|v| *v as u64).product()
-        );
+        let _result = solve(tiles);
+        assert_eq!(0, 1);
     }
 
     // #[test]
