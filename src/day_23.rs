90eb3cdb94cc7c2f60ef996da11e7d5b2f29ab31
Paul Doyle <pauldoyle22@gmail.com>
2020-12-23 03:06:51 -0800

    A much more reasonable approach; Day 23 P2 working

--- src/day_23.rs
+++ src/day_23.rs
@@ -1,174 +1,87 @@
-use std::{
-    cell::{RefCell, RefMut},
-    collections::HashMap,
-    rc::Rc,
-};
+static INPUT: [usize; 9] = [4, 6, 7, 5, 2, 8, 1, 9, 3];
 
-static INPUT: [u32; 9] = [4, 6, 7, 5, 2, 8, 1, 9, 3];
-
-fn move_cups(cc: usize, cups: &mut Vec<u32>, max: u32) {
-    let len = cups.len();
-    let label = cups[cc];
-    let picked_up = (
-        std::mem::replace(&mut cups[(cc + 1) % len], 0),
-        std::mem::replace(&mut cups[(cc + 2) % len], 0),
-        std::mem::replace(&mut cups[(cc + 3) % len], 0),
-    );
-    let mut dest = if label > 1 { label - 1 } else { max };
-    while dest == picked_up.0 || dest == picked_up.1 || dest == picked_up.2 {
-        dest -= 1;
-        if dest == 0 {
-            dest = max;
-        }
-    }
-
-    let mut counter = 0;
-    loop {
-        let index = (cc + 1 + counter) % len;
-        cups[index] = cups[(index + 3) % len];
-        if cups[(index + 3) % len] == dest {
-            cups[(index + 1) % len] = picked_up.0;
-            cups[(index + 2) % len] = picked_up.1;
-            cups[(index + 3) % len] = picked_up.2;
-            break;
-        }
-        counter += 1;
-    }
-}
-
-#[derive(Debug)]
-struct Node {
-    value: u32,
-    next: Option<Rc<RefCell<Node>>>,
-}
-
-impl Node {
-    fn next_value(&self) -> u32 {
-        self.next
-            .as_ref()
-            .map(|node| node.try_borrow().unwrap().value)
-            .unwrap()
-    }
-}
-
-#[derive(Debug)]
 struct Cups {
-    nodes: HashMap<u32, Rc<RefCell<Node>>>,
+    max: usize,
+    nodes: Vec<usize>,
 }
 
 impl Cups {
-    fn from_input(input: &[u32], extend: usize) -> Self {
-        let mut new_map = Self {
-            nodes: HashMap::new(),
-        };
-
-        new_map.nodes.insert(
-            input[0],
-            Rc::new(RefCell::new(Node {
-                value: input[0],
-                next: None,
-            })),
-        );
-        let mut prev_value = input[0];
-        for i in 1..extend {
-            let value = if i < input.len() {
-                input[i]
-            } else {
-                (i + 1) as u32
-            };
-            let node = Rc::new(RefCell::new(Node { value, next: None }));
-            new_map.nodes.insert(value, node.clone());
-            new_map.get_mut(&prev_value).next = Some(node);
-            prev_value = value;
+    fn from_input(input: &[usize], extend: usize) -> Self {
+        let mut nodes = vec![0; input.len() + 1];
+        // There will be a dummy value (0) at index 0, which is honestly just easier
+        for i in 0..input.len() - 1 {
+            nodes[input[i]] = input[i + 1];
         }
-        println!("Size: {}", new_map.nodes.len());
+        if extend > 0 {
+            for i in input.len()..extend - 1 {
+                nodes.push(i + 2);
+            }
+            nodes[input[input.len() - 1]] = input.len() + 1;
 
-        new_map
+            nodes.push(input[0]);
+        } else {
+            nodes[input[input.len() - 1]] = input[0];
+        }
+
+        Self {
+            max: std::cmp::max(input.len(), extend),
+            nodes,
+        }
     }
 
-    fn next_value(&self, value: &u32) -> u32 {
-        self.nodes
-            .get(value)
-            .unwrap()
-            .try_borrow()
-            .unwrap()
-            .next_value()
-    }
+    fn move_three(&mut self, current: usize) {
+        let mut dest = if current > 1 { current - 1 } else { self.max };
+        let first = self.nodes[current];
+        let second = self.nodes[first];
+        let third = self.nodes[second];
+        while dest == first || dest == second || dest == third {
+            dest -= 1;
+            if dest < 1 {
+                dest = self.max;
+            }
+        }
 
-    fn three_away(&self, value: &u32) -> RefMut<'_, Node> {
-        let first_next = self.next_value(value);
-        let second_next = self.next_value(&first_next);
-        self.get_mut(&second_next)
-    }
-
-    fn get_mut(&self, value: &u32) -> RefMut<'_, Node> {
-        let node = self.nodes.get(value).unwrap();
-        node.borrow_mut()
-    }
-
-    fn move_three(&mut self, current: u32, dest: u32) {
-        let mut current_node = self.get_mut(&current);
-        let mut dest_node = self.get_mut(&dest);
-
-        let mut third_node = self.three_away(&current);
-
-        std::mem::swap(&mut current_node.next, &mut third_node.next);
-        std::mem::swap(&mut third_node.next, &mut dest_node.next);
+        self.nodes.swap(current, third);
+        self.nodes.swap(third, dest);
     }
 }
 
-fn play_game_p1(cups: &mut Vec<u32>) {
-    let max = *cups.iter().max().unwrap();
-    for cc in 0..100 {
-        move_cups(cc % cups.len(), cups, max)
+fn play_game(initial_cups: &[usize], extend: usize, moves: usize) -> Cups {
+    let mut cups = Cups::from_input(initial_cups, extend);
+    let mut current = initial_cups[0];
+    for _ in 0..moves {
+        cups.move_three(current);
+        current = cups.nodes[current];
     }
+    cups
 }
 
-fn get_result_p1(cups: &Vec<u32>) -> String {
-    let len = cups.len();
-    let start = cups.iter().position(|v| *v == 1).unwrap() + 1;
+fn get_result_p1(cups: &Cups) -> String {
     let mut result = String::new();
-    for counter in 0..len - 1 {
-        let index = (start + counter) % len;
-        result.push_str(cups[index].to_string().as_str());
+    let mut next = cups.nodes[1];
+    loop {
+        result.push_str(next.to_string().as_str());
+        next = cups.nodes[next];
+        if next == 1 {
+            break;
+        }
     }
     result
 }
 
-fn get_p2_input(starting_input: &[u32]) -> Vec<u32> {
-    let mut result: Vec<u32> = starting_input.iter().copied().collect();
-    for i in starting_input.len()..1_000_000 {
-        result.push((i + 1) as u32);
-    }
-
-    result
-}
-
-fn play_game_p2(cups: &mut Vec<u32>) {
-    let max = *cups.iter().max().unwrap();
-    // for cc in 0..10_000_000 {
-    for cc in 0..500 {
-        move_cups(cc % cups.len(), cups, max)
-    }
-}
-
-fn get_result_p2(cups: &Vec<u32>) -> u64 {
-    let len = cups.len();
-    let one_pos = cups.iter().position(|v| *v == 1).unwrap();
-    let a = cups[(one_pos + 1) % len];
-    let b = cups[(one_pos + 2) % len];
+fn get_result_p2(cups: &Cups) -> u64 {
+    let a = cups.nodes[1];
+    let b = cups.nodes[a];
     (a as u64) * (b as u64)
 }
 
 pub fn p1() -> String {
-    let mut cups = INPUT.iter().copied().collect();
-    play_game_p1(&mut cups);
+    let cups = play_game(&INPUT, 0, 100);
     get_result_p1(&cups)
 }
 
 pub fn p2() -> u64 {
-    let mut cups = get_p2_input(&INPUT);
-    play_game_p2(&mut cups);
+    let cups = play_game(&INPUT, 1_000_000, 10_000_000);
     get_result_p2(&cups)
 }
 
@@ -176,37 +89,29 @@ pub fn p2() -> u64 {
 mod test {
     use super::*;
 
-    static EXAMPLE: [u32; 9] = [3, 8, 9, 1, 2, 5, 4, 6, 7];
-
-    #[test]
-    fn ridiculous_hash_approach() {
-        let test = Cups::from_input(&EXAMPLE, 10_000);
-        assert_eq!(0, 1);
-    }
+    static EXAMPLE: [usize; 9] = [3, 8, 9, 1, 2, 5, 4, 6, 7];
 
     #[test]
     fn p1_example() {
-        let mut cups = EXAMPLE.iter().copied().collect();
-        play_game_p1(&mut cups);
+        let cups = play_game(&EXAMPLE, 0, 100);
         assert_eq!("67384529", get_result_p1(&cups));
     }
 
     #[test]
     fn p1_correct_answer() {
-        let mut cups = INPUT.iter().copied().collect();
-        play_game_p1(&mut cups);
+        let cups = play_game(&INPUT, 0, 100);
         assert_eq!("43769582", get_result_p1(&cups));
     }
 
     #[test]
-    #[ignore = "solution too slow, not ready yet"]
     fn p2_example() {
-        let mut cups = get_p2_input(&EXAMPLE);
-        play_game_p2(&mut cups);
+        let cups = play_game(&EXAMPLE, 1_000_000, 10_000_000);
         assert_eq!(149245887792, get_result_p2(&cups));
     }
 
-    // #[test]
-    // fn p2_correct_answer() {
-    // }
+    #[test]
+    fn p2_correct_answer() {
+        let cups = play_game(&INPUT, 1_000_000, 10_000_000);
+        assert_eq!(264692662390, get_result_p2(&cups));
+    }
 }
